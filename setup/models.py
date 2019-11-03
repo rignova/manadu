57d8343fefd719b9fc1e506dfc83bc1af29a4462
Rajkumar R <rajkumar.mailme@gmail.com>
2019-11-03 16:51:14 +0530

    Added Conference model to setup.

--- setup/models.py
+++ setup/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Conference(models.Model):
+    name = models.CharField(max_length=50)
+    admin_email = models.EmailField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
